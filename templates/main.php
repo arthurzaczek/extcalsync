6a234def7b6926e866fbb0de78defba9b37a3de0
Arthur Zaczek <arthur@dasz.at>
2014-03-29 19:48:08 +0100

    Basic UI Layout

--- templates/main.php
+++ templates/main.php
@@ -1,1 +1,14 @@
-<p><?php p($_['msg']); ?></p>
+<div id="app">
+	<div id="app-navigation">
+		<ul id="grouplist">
+			<li class="special">
+				<a role="button" class="">
+					+ <?php p($l->t('New external Calendar')); ?>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div id="app-content">
+		<p><?php p($_['msg']); ?></p>
+	</div>
+</div>
